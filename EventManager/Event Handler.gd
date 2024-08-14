3dd4ee6a13876f0b38bd98b7f6a113a662fce33e
Deihun <109403575+Deihun@users.noreply.github.com>
2024-08-14 19:29:28 +0800

    EVENT READER UPDATE
    *Added few traits to Event Reader Classes*
    + FIXED Repeatable: now working properly
    + ADDED 'Conditions': [ effect_type , [ effect_name ] ]//Check the chosen event if conditions are met before adding on the queue EventHandler
    + ADDED 'Choice-*number*':[id_target, [item_name , item_value//This is subtract immediately if the value is sufficient//] button_description] //Add buttons and handles multiple functions of a button being press. Use it to add choices to events
    + ADDED 'Probability': [probability_var, [COMMAND]] // Use for handling randomness in event
    + ADDED 'Command': [command] // Commands are not instant but interchangable, offering manipulations of data base on given instructions. ex @RESET Spareparts - Cause spareparts to be reset to zero
    +ADDED 'FollowUp': [Follow_up_name_key] // FollowUp are use to add event key, other key tag with follow up will be prioritize to be queue next
    
    +ADDED End Cycle as a button (TEMPORARY TEXTURE)
    +UPDATE GlobalResources
    ++ Add few new functions for data manipulation
    ++ Add UniqueItem and GameEffect for array storing.
    + UPDATE Camera now switch position into event_image when ending Cycle
    + UPDATE Event_UI will now disappear if no event is on queue. (NEEDS ANIMATION ASSETS FOR THIS ONE)
    
    + UPDATE Event_json //Events here are temporarilly for testing, not the event implementation for the final game

---
+++ EventManager/Event Handler.gd
@@ -0,0 +1,86 @@
+extends Node2D
+
+@onready var eventReader = $Event_UI
+@onready var Cycle = $"/root/IngameStoredProcessSetting"
+
+
+var rawEvent = []
+var alreadyTriggeredEvent = []
+var Priority_Event = []
+var eventID = []
+
+
+func _ready():
+	var file_path = "res://Scripts/Events.json"
+	if(FileAccess.file_exists(file_path)):
+		var file = FileAccess.open(file_path,FileAccess.READ)
+		var json_text = file.get_as_text()
+		rawEvent = parse_json(json_text)
+	else: 
+		print("EVENT-HANDLER-SCRIPT://  'func _ready()' : 'Failed to locate'")
+
+
+func parse_json(json_text):
+	var json = JSON.new()
+	var result = json.parse(json_text)
+	if result != OK:
+		print("EVENT-HANDLER-SCRIPT://  'parse_json' : 'Failed to analyze json file'", json.get_error_line())
+		return []
+	return json.get_data()
+
+
+func _process(delta):
+	pass
+
+func _newGameStart():
+	pass
+
+func _loadGameStart(eventLoads):
+	pass
+
+func startAddNextEvent():
+	self.visible = true
+	var numbers_of_event =  int(2 + (randf() * (Cycle.getCycle()*0.03)))
+	
+	while(Priority_Event.size() > 0 && numbers_of_event > 0):
+		var priority_key = Priority_Event.pop_front()
+		for eachEvent in rawEvent:
+			if eachEvent.has("Conditions"):
+				if eachEvent["Conditions"][0] == "FOLLOW-UP" && eachEvent["Conditions"][1].has(priority_key):
+					eventID.append(eachEvent["id"])
+					numbers_of_event -= 1
+	
+	for i in numbers_of_event:
+		_addNextEvent()
+
+func _addNextEvent():
+	var event_index_random = randi() % rawEvent.size()
+	var event = rawEvent[event_index_random]
+	if event["RandomTrue"] != true:
+		_addNextEvent()
+		return
+	if event["Repeatable"] == false:
+		if !alreadyTriggeredEvent.has(event["id"]):
+			alreadyTriggeredEvent.append(event["id"])
+		else:
+			_addNextEvent()
+			return
+	eventID.append(event["id"])
+
+
+func ActivateEvent():
+	if eventID.front() == null:
+		self.visible = false
+		return
+	eventReader.setEventID(eventID.pop_front())
+	eventReader.processNextEvent()
+
+func _removeAllEvent():
+	eventID.clear()
+
+#RETURNING METHODS
+func getAllEventIDHierarchy():
+	eventReader.setEventID(eventID)
+
+func addFollowUpEvent(event):
+	Priority_Event.append(event)
--- Resources/GlobalResources.gd
+++ Resources/GlobalResources.gd
@@ -3,10 +3,11 @@ extends Node
 #VARIABLES
 var food = 0
 var fuel = 0
-var spareparts = 0
+var spareparts = 50
 var biogene = 0
 var ductape = 0
-
+var GameEffects = [] 
+var uniqueItems = []
 
 func add_item(item_type):#Use to direct add items, used by other objects such as dropbox to add items
 	match(item_type):
@@ -26,3 +27,27 @@ func add_item(item_type):#Use to direct add items, used by other objects such as
 
 func showTotalItems(): #For DEBUG only
 	print("Food: ",food," Fuel: ", fuel," Spareparts: ", spareparts, " biogene: ", biogene, " Ductape: ", ductape)
+
+func getSpareparts():
+	return spareparts
+
+func subtractItem(conditions,item_name, amount):
+	if conditions:
+		match(item_name):
+			"spareparts":
+				spareparts -= amount
+
+func reset(item_type):
+	match(item_type):
+		"Spareparts":
+			spareparts = 0
+
+func addGameEffect(name):
+	GameEffects.append(name)
+
+func checkEffect(effect_name):
+	return GameEffects.has(effect_name)
+
+func removeEffect(effect_name):
+	GameEffects.erase(effect_name)
+
