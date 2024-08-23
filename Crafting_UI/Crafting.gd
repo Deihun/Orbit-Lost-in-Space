b173a5b6c2320e10b7dbe36a8f52a4ac17921cc5
Neko014 <89933102+Neko014@users.noreply.github.com>
2024-08-24 01:59:05 +0800

    Added value adding to uniqueItems and bug fix
    added append to unique Items in global resource and fixed some logical errors from previous commit

--- Crafting_UI/Crafting.gd
+++ Crafting_UI/Crafting.gd
@@ -6,70 +6,128 @@ extends Node
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 var craftingItems
 
-func craft():
+func craft_button_pressed() -> void:
 	match craftingItems:
 		"SpaceSuit":
-			if getResources.spareparts >= 300 && getResources.oxygen <= 100 && getResources.fuel <= 10:
+			if getResources.spareparts >= 300 && getResources.oxygen >= 100 && getResources.fuel >= 10:
 				getResources.spareparts -= 300
 				getResources.oxygen -= 100
 				getResources.fuel -= 10
+				getResources.uniqueItems.append(craftingItems)
 				print("Successfully Made!")
 			else:
 				print("Insufficient Items!")
 			
 		"HazmatSuit":
-			if getResources.spareparts >= 400 && getResources.fuel <= 10:
+			if getResources.spareparts >= 400 && getResources.fuel >= 10:
 				getResources.spareparts -= 400
 				getResources.fuel -= 10
+				getResources.uniqueItems.append(craftingItems)
 				print("Successfully Made!")
 			else:
 				print("Insufficient Items!")
 			
 		"Crowbar":
-			if getResources.spareparts >= 350 && getResources.fuel <= 10:
+			if getResources.spareparts >= 350 && getResources.fuel >= 10:
 				getResources.spareparts -= 350
 				getResources.fuel -= 10
+				getResources.uniqueItems.append(craftingItems)
 				print("Successfully Made!")
 			else:
 				print("Insufficient Items!")
 			
 		"LaserGun":
-			if getResources.spareparts >= 350 && getResources.biogene >= 20 && getResources.fuel <= 10:
+			if getResources.spareparts >= 350 && getResources.biogene >= 20 && getResources.fuel >= 10:
 				getResources.spareparts -= 350
 				getResources.biogene -= 20
 				getResources.fuel -= 10
+				getResources.uniqueItems.append(craftingItems)
 				print("Successfully Made!")
 			else:
 				print("Insufficient Items!")
 			
 		"MedkitCharge":
-			if getResources.biogene >= 150 && getResources.fuel <= 10:
+			if getResources.biogene >= 150 && getResources.fuel >= 10:
 				getResources.biogene -= 150
 				getResources.fuel -= 10
+				getResources.uniqueItems.append(craftingItems)
 				print("Successfully Made!")
 			else:
 				print("Insufficient Items!")
 			
 		"DehySpaceFood":
-			if getResources.biogene >= 100 && getResources.fuel <= 10:
+			if getResources.biogene >= 100 && getResources.fuel >= 10:
 				getResources.biogene -= 150
 				getResources.fuel -= 10
+				getResources.uniqueItems.append(craftingItems)
 				print("Successfully Made!")
 			else:
 				print("Insufficient Items!")
 			
 		"FreDriSpaceFood":
-			if getResources.ration >= 20 && getResources.fuel <= 10:
+			if getResources.ration >= 20 && getResources.fuel >= 10:
 				getResources.ration -= 20
 				getResources.fuel -= 10
+				getResources.uniqueItems.append(craftingItems)
 				print("Successfully Made!")
 			else:
 				print("Insufficient Items!")
+		_:
+			print("Select a Item")
 	pass
+
+
+func add_res_pressed() -> void:
+	getResources.spareparts += 1000
+	getResources.oxygen += 1000
+	getResources.fuel += 1000
+	getResources.biogene += 1000
+	getResources.ration += 1000
+	print(getResources.spareparts, " ", getResources.oxygen, " ", getResources.fuel, " ", getResources.biogene, " ", getResources.ration)
+	pass # Replace with function body.
+
+func check_res_pressed() -> void:
+	print(getResources.spareparts, " ", getResources.oxygen, " ", getResources.fuel, " ", getResources.biogene, " ", getResources.ration)
+	print(getResources.uniqueItems)
+	pass # Replace with function body.
+
+func space_suit_pressed() -> void:
+	craftingItems = "SpaceSuit"
+	print("Selected Space suit")
+	pass # Replace with function body.
+
+func crowbar_pressed() -> void:
+	craftingItems = "Crowbar"
+	print("Selected Crowbar")
+	pass # Replace with function body.
+
+func laser_gun_pressed() -> void:
+	craftingItems = "LaserGun"
+	print("Selected Laser Gun")
+	pass # Replace with function body.
+
+func _on_dehy_space_food_pressed() -> void:
+	craftingItems = "DehySpaceFood"
+	print("Selected DehySpaceFood")
+	pass # Replace with function body.
+
+func medkit_charge_pressed() -> void:
+	craftingItems = "MedkitCharge"
+	print("Selected MedkitCharge")
+	pass # Replace with function body.
+
+func fre_dri_space_food_pressed() -> void:
+	craftingItems = "FreDriSpaceFood"
+	print("Selected FreDriSpaceFood")
+	pass # Replace with function body.
+
+func hazmat_suit_pressed() -> void:
+	craftingItems = "HazmatSuit"
+	print("Selected HazmatSuit")
+	pass # Replace with function body.
