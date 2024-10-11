138026f493f321dc21791d212e633692a8696df6
Deihun <109403575+Deihun@users.noreply.github.com>
2024-10-11 15:02:58 +0800

    Testing Commit

--- DialogueLoader.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-
-@export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "start"
-
-function dialogue() -> void:
-	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
