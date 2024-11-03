f053db2001749a1f5bf80cea0453fd1882bfc4a3
Neko014 <mavememije@gmail.com>
2024-11-03 19:08:07 +0800

    bug fixes
    + player move blur fixed
    + fixed tab move sound

--- settings_menu/PlaySoundtabs.gd
+++ settings_menu/PlaySoundtabs.gd
@@ -2,14 +2,5 @@ extends Node
 
 @onready var button_sfx: AudioStreamPlayer = $"../../ButtonSfx"
 
-func _on_graphics_visibility_changed() -> void:
-	button_sfx.play()
-
-func _on_visibility_changed() -> void:
-	button_sfx.play()
-
-func _on_controls_visibility_changed() -> void:
-	button_sfx.play()
-
-func _on_general_visibility_changed() -> void:
+func _on_tab_container_tab_changed(tab: int) -> void:
 	button_sfx.play()
