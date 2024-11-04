9bb9d37ba2302edff7af7d580887293647e951f9
Neko014 <mavememije@gmail.com>
2024-11-05 02:23:10 +0800

    Fix Resolution

--- settings_menu/window_mode_button.gd
+++ settings_menu/window_mode_button.gd
@@ -1,6 +1,9 @@
 extends Control
 
 @onready var option_button = $HBoxContainer/OptionButton as OptionButton
+@onready var resolution_mode_button: Control = NodeFinder.find_node_by_name(get_tree().current_scene, "Resolution_Mode_Button")
+
+var fullmode
 
 const WINDOW_MODE_ARRAY : Array[String] = [
 	"Full-Screen",
@@ -28,12 +31,31 @@ func on_window_mode_selected(index : int) -> void:
 		0: #fullscreen
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			fullmode = true
 		1: #window mode
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			fullmode = false
 		2: #Borderless Window
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
-		3: #Borderless Window
+			fullmode = false
+		3: #Borderless Window full
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+			fullmode = true
+
+func _on_option_button_item_selected(index: int) -> void:
+	match index:
+		0:
+			fullmode = true
+			resolution_mode_button.checkResolution()
+		1:
+			fullmode = false
+			resolution_mode_button.checkResolution()
+		2:
+			fullmode = false
+			resolution_mode_button.checkResolution()
+		3:
+			fullmode = true
+			resolution_mode_button.checkResolution()
